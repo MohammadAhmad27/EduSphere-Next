3f9b8dd88475ca0c6bec901cd692472be2896997
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2025-01-03 23:52:48 +0500

    Completed /list/teachers route and Added <TableSearch />, <Table /> &<Pagination />

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,6 +1,5 @@
 import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
-import EventCalendar from "@/components/EventCalendar";
 
 const StudentPage = () => {
   return (
@@ -8,13 +7,12 @@ const StudentPage = () => {
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
         <div className="h-full rounded-md p-4 bg-white">
-          <h1 className="txt-xl font-semibold">Schedule</h1>
+          <h1 className="txt-xl font-semibold">Schedule (John Doe)</h1>
           <BigCalendar />
         </div>
       </div>
       {/* RIGHT */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
         <Announcements />
       </div>
     </div>
